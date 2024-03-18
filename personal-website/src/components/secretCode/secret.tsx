5d2d5236b3a0b0ee46391f084a0f476c954178bf
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-03-18 15:00:33 -0400

    added dune2 music after activating konami code

--- personal-website/src/components/secretCode/secret.tsx
+++ personal-website/src/components/secretCode/secret.tsx
@@ -1,12 +1,20 @@
 import { useKonamiCode } from "./useKonamiCode";
 import { useEffect } from "react";
+import sound from "/src/assets/Dune_ Part Two Soundtrack _ Kiss the Ring - Hans Zimmer _ WaterTower [ ytmp3x.cc ].mp3";
 
 export const Secret = () => {
   const konami = useKonamiCode();
 
+  function play() {
+    new Audio(sound).play();
+  }
+
   useEffect(() => {
-    if (konami) console.log("Konami code activated");
-    // only renders when konami value is changed
+    if (konami) {
+      console.log("Konami code activated");
+      play();
+    }
+    // only renders when konami value is changed the first time
   }, [konami]);
 
   return null;
