7bea71ae987261a8365873cbf0afa3dc5403b460
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-04-10 16:31:40 -0400

    added dialog box when clicing on groove generator in header

--- personal-website/src/components/header.tsx
+++ personal-website/src/components/header.tsx
@@ -1,11 +1,33 @@
+// import { useEffect, useRef } from "react";
+
 const Header = () => {
+  // add more dialog boxes for the rest
+  window.onload = function () {
+    const dialog = document.querySelector("dialog")!;
+    const openDialog = document.getElementById("groove-generator")!;
+    const closeDialog = document.querySelector("dialog button");
+    openDialog.addEventListener("click", () => {
+      dialog.showModal();
+    });
+    closeDialog?.addEventListener("click", () => {
+      dialog.close();
+    });
+  };
+
   return (
     <div id="header" className="header">
-      <a href="https://www.groovegenerator.xyz/">
+      {/* <a href="https://www.groovegenerator.xyz/"> */}
+      <button>
         <h2 id="groove-generator" className="groove-generator header-elements">
           Groove Generator
         </h2>
-      </a>
+      </button>
+      <dialog>
+        <button className="dialog-close-btn">Close</button>
+        <img alt="image of groove generator website"></img>
+        <p>groove</p>
+      </dialog>
+      {/* </a> */}
 
       <a href="https://focus-4-you.surge.sh/">
         <h2 id="focus4you" className="focus-4-you header-elements">
