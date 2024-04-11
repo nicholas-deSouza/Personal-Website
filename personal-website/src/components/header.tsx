8e77d457a190b1d9602a690eb3bfb6572f5eee15
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-04-11 17:16:19 -0400

    added dialog box for focus4you

--- personal-website/src/components/header.tsx
+++ personal-website/src/components/header.tsx
@@ -3,37 +3,70 @@
 const Header = () => {
   // add more dialog boxes for the rest
   window.onload = function () {
-    const dialog = document.querySelector("dialog")!;
-    const openDialog = document.getElementById("groove-generator")!;
-    const closeDialog = document.querySelector("dialog button");
-    openDialog.addEventListener("click", () => {
-      dialog.showModal();
+    // dialog boxes for header elements
+    const dialogGrooveGenerator = document.getElementById("groove-generator-dialog")! as HTMLDialogElement;
+    const dialogFocus4You = document.getElementById("focus4you-dialog")! as HTMLDialogElement;
+
+    const openDialogGrooveGenerator = document.getElementById("groove-generator")!;
+    const openDialogFocus4You = document.getElementById("focus4you")!;
+
+    const closeDialogGrooveGenerator = document.getElementById("groove-generator-dialog-btn-id");
+    const closeDialogFocus4You = document.getElementById("focus4you-dialog-btn-id");
+
+    openDialogGrooveGenerator.addEventListener("click", () => {
+      dialogGrooveGenerator.showModal();
     });
-    closeDialog?.addEventListener("click", () => {
-      dialog.close();
+    openDialogFocus4You.addEventListener("click", () => {
+      dialogFocus4You.showModal();
+    });
+
+    closeDialogGrooveGenerator?.addEventListener("click", () => {
+      dialogGrooveGenerator.close();
+    });
+
+    closeDialogFocus4You?.addEventListener("click", () => {
+      dialogFocus4You.close();
     });
   };
 
   return (
     <div id="header" className="header">
       {/* <a href="https://www.groovegenerator.xyz/"> */}
-      <button>
+      <button id="groove-generator-id" className="groove-generator-btn">
         <h2 id="groove-generator" className="groove-generator header-elements">
           Groove Generator
         </h2>
       </button>
-      <dialog>
-        <button className="dialog-close-btn">Close</button>
-        <img alt="image of groove generator website"></img>
-        <p>groove</p>
+
+      <dialog id="groove-generator-dialog">
+        <button id="groove-generator-dialog-btn-id" className="dialog-close-btn">
+          Close
+        </button>
+        <img src="src/assets/groove-generator.png" alt="image of groove generator website"></img>
+        <h3>A website that allows for Spotify playlist generation with by just giving a phrase!</h3>
       </dialog>
       {/* </a> */}
 
-      <a href="https://focus-4-you.surge.sh/">
+      {/* <a href="https://focus-4-you.surge.sh/"> */}
+
+      <button id="focus4you-id" className="focus4you-btn">
         <h2 id="focus4you" className="focus-4-you header-elements">
           Focus4You
         </h2>
-      </a>
+      </button>
+
+      <dialog id="focus4you-dialog">
+        <button id="focus4you-dialog-btn-id" className="dialog-close-btn">
+          Close
+        </button>
+        <img src="src/assets/focus4u.png" alt="image of focus4you website"></img>
+        <h3>
+          A one-stop location on the web focused on improving your productivity by incorporating features such as a
+          pomodoro timer and a heatmap calendar!
+        </h3>
+      </dialog>
+
+      {/* </a> */}
 
       <a href="">
         <h2 id="contact" className="contact header-elements">
