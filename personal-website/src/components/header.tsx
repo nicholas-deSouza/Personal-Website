5b54425fc40162a2ed2745ec6a21ae434812dfd4
Nicholas de Souza <nrdesouzag13@gmail.com>
2024-04-30 14:55:44 -0400

    changed buttons to a tags

--- personal-website/src/components/header.tsx
+++ personal-website/src/components/header.tsx
@@ -1,43 +1,21 @@
-// const Header = () => {
-//   return (
-//     <div id="header" className="header">
-//       <a href="https://www.groovegenerator.xyz/">
-//         <button id="groove-generator-id" className="groove-generator-btn">
-//           <h2 id="groove-generator" className="groove-generator header-elements">
-//             Groove Generator
-//           </h2>
-//         </button>
-//       </a>
-
-//       <a href="https://focus-4-you.surge.sh/">
-//         <button id="focus4you-id" className="focus4you-btn">
-//           <h2 id="focus4you" className="focus-4-you header-elements">
-//             Focus4You
-//           </h2>
-//         </button>
-//       </a>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
 const Header = () => {
   return (
     <div id="header" className="header">
       <ul className="header-list">
         <li className="header-item">
-          <a href="https://www.groovegenerator.xyz/">
-            <h2 id="groove-generator" className="groove-generator header-elements">
+          <a id="groove-generator" className="groove-generator header-elements" href="https://www.groovegenerator.xyz/">
+            {/* <h2 id="groove-generator" className="groove-generator header-elements">
               Groove Generator
-            </h2>
+            </h2> */}
+            Groove Generator
           </a>
         </li>
         <li className="header-item">
-          <a href="https://focus-4-you.surge.sh/">
-            <h2 id="focus4you" className="focus-4-you header-elements">
+          <a id="focus4you" className="focus-4-you header-elements" href="https://focus-4-you.surge.sh/">
+            {/* <h2 id="focus4you" className="focus-4-you header-elements">
               Focus4You
-            </h2>
+            </h2> */}
+            Focus4You
           </a>
         </li>
       </ul>
